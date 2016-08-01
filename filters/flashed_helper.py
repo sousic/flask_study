c90933b939a68f2699617296afb9d9ef8418100e
sousic <sousics@naver.com>
2016-08-01 16:33:41 +0900

    jinja2 custom filter -> class로 변경

--- filters/flashed_helper.py
+++ filters/flashed_helper.py
@@ -5,10 +5,13 @@
 별다른 인덱스값이 없으면 0번으로 고정 보통 1개만 넣기때문에
 """
 class flashed_helper:
+    app = ''
     def __init__(self, app):
         app.jinja_env.filters['flashed_messages_helper'] = self.flashed_messages_helper
+        self.app = app
 
     def flashed_messages_helper(self, value, index = 0):
+        self.app.logger.debug('flashed_message length : %s', len(value))
         if len(value) > 0:
             return value[index]
         return ''
